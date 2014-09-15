8ac8e86d1a8fefe1178e5c7bc92d8b89f6b24c36
Peter Jones <pjones15@gmu.edu>
2014-09-14 23:02:52 -0400

    add html file and range function

--- sum-range.js
+++ sum-range.js
@@ -1,8 +1,13 @@
 /* Sum of a Range*/
-Range{}
+function range(start, end){
+	var total=[start]
+	for(var i=start; i=>end; i++)
+		i+1=total.push;
+	return total
+}
 
-Sum{}
+function Sum(){}
 
+console.log(range(1,10))
 
-
-console.log(sum(range(1, 10)));
+//console.log(sum(range(1, 10)));
