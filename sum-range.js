67a981268e3087da6407b8d92d6424d3c3d544db
Peter Jones <pjones15@gmu.edu>
2014-09-14 23:25:28 -0400

    another attempt

--- sum-range.js
+++ sum-range.js
@@ -4,8 +4,9 @@ var total=[]
 
 function range(start,end){
 	total.push (start)
-	for(var i = start; i > end; i++)
-		total.push=[i];
+	for(var i = start; i >= end; i++)
+		(start+1)===start
+		total.push=[i+1];
 	return total
 }
 
