f1e6b8e614a3fcfb30136a0dcf3e48f779f5491e
Peter Jones <pjones15@gmu.edu>
2014-09-16 01:38:17 -0400

    grammar fix

--- reduce.js
+++ reduce.js
@@ -6,8 +6,8 @@ var arrays = [[1, 2, 3], [4, 5], [6]];
   var current = start;
   for (var i = 0; i < array.length; i++)
     current = combine(current, array[i]);
-  return current;*/
-}
+  return current;
+}*/
 arrays.reduce(function(combine,start){
 	return combine.concat.start;
 }
