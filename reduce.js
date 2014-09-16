1c6db54b086faabb707a7a58f6a8840dac75632b
Peter Jones <pjones15@gmu.edu>
2014-09-16 01:37:05 -0400

    use reduce and concat function and add console.log

--- reduce.js
+++ reduce.js
@@ -2,4 +2,13 @@
 
 var arrays = [[1, 2, 3], [4, 5], [6]];
 
-function range(start, end, step){
+/*function reduce(array, combine, start) {
+  var current = start;
+  for (var i = 0; i < array.length; i++)
+    current = combine(current, array[i]);
+  return current;*/
+}
+arrays.reduce(function(combine,start){
+	return combine.concat.start;
+}
+console.log(arrays)
