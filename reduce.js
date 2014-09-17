1d25b340ea66c6b8ce1e4a6cb1a1361c358c5b77
Peter Jones <pjones15@gmu.edu>
2014-09-17 00:21:45 -0400

    clean up bad code and write victory lap comments

--- reduce.js
+++ reduce.js
@@ -8,15 +8,5 @@ flatArray = function(ary){
 
 var arrays = [[1, 2, 3], [4, 5], [6]];
 
-/*function reduce(arrays, concat, start) {
-  var current = start;
-  for (var i = 0; i < arrays.length; i++)
-    current = concat(current, arrays[i]);
-  return current;
-}
-*/
-
-/*arrays.reduce(function(combine,start){
-	return combine.concat(start);
-})*/
+/*The problem was not creating a separate function to call the "reduce" method. Ahhhh.*/
 console.log(flatArray(arrays))
